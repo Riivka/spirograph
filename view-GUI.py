e0cdc9a58a24c416e4e12e7b285d771d272373ee
Riivka <rivkaover@gmail.com>
2022-06-26 18:51:26 +0300

    view

--- view-GUI.py
+++ view-GUI.py
@@ -1,3 +1,4 @@
+from turtle import color
 import PySimpleGUI as sg
 from spirograph import Spiro
 
@@ -11,10 +12,14 @@ SIZE_Y = GRAPH_SIZE[1]//2
 
 graph = sg.Graph(GRAPH_SIZE, (0, 0), DATA_SIZE, key='-GRAPH-', background_color='white',)
 
-layout = [[sg.Text('enter radius....')],
-          [graph],
-          [sg.Button('draw')]]
-window = sg.Window(title="spirograph", layout=layout)
+layout = [[sg.Text('enter', background_color='#FCFCFC', text_color='black')],
+          [sg.Text('R', size=(15, 1), background_color='#FCFCFC',justification='right', text_color='black'), sg.Input(key='R_input', size=(15, 1))],
+          [sg.Text('r', size=(15, 1), background_color='#FCFCFC', justification='right', text_color='black'), sg.Input(key='r_input', size=(15, 1))],
+          [sg.Text('l', size=(15, 1), background_color='#FCFCFC', justification='right', text_color='black'), sg.Input(key='l_input', size=(15, 1))],
+          [sg.Button('draw')],
+          [graph]]
+          
+window = sg.Window(title="spirograph", layout=layout, background_color='#FCFCFC')
 
 
 while True:
@@ -31,7 +36,7 @@ while True:
         graph.erase()
 
         #draw_axis()
-        spirog = Spiro(200, 170, 0.6, SIZE_X, SIZE_Y)
+        spirog = Spiro(int(values['R_input']),int(values['r_input']), float(values['l_input']), SIZE_X, SIZE_Y)
         plot = spirog.draw()
         prev_x, prev_y = None, None
         for x,y in plot:
